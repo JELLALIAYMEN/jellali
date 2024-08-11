c95bf74b1a71cc3040ee177eb945f4324415addd
unknown <Aymen.jellali@esprit.tn>
2024-08-11 21:56:03 +0100

    Gateway

---
+++ src/main/java/com/example/gatewayaa/GatewayaaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewayaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayaaApplication.class, args);
+	}
+
+}
