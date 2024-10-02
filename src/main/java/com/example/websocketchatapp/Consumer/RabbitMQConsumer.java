0824c3323ea6abf765ea65d41adb14552e0ca0e3
Aymen <Aymen.jellali@esprit.tn>
2024-10-02 21:33:42 +0100

    Initial commit

--- src/main/java/com/example/websocketchatapp/Consumer/RabbitMQConsumer.java
+++ src/main/java/com/example/websocketchatapp/Consumer/RabbitMQConsumer.java
@@ -1,16 +1,34 @@
 package com.example.websocketchatapp.Consumer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Service;
+import lombok.Value;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@Service
-public class RabbitMQConsumer {
-    public static  final Logger LOGGER= LoggerFactory.getLogger(RabbitMQConsumer.class);
-    @RabbitListener(queues = {"${rabbitmq.queue.name}"})
-    public  void  consume(String message){
-        LOGGER.info(String.format("Receivedmessage-> %s", message));
+@Configuration
+public class RabbitMQConfig {
+
+    private String queueName;
+
+       private String exchange;
+
+
+    private String routingKey;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, true);  // Assurez-vous que la file d'attente est durable
     }
 
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchange);
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
 }
