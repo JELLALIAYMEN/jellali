eae99f2ab4d6cd2afaff2fc3f3898357eaee508b
Aymen <Aymen.jellali@esprit.tn>
2024-10-02 20:56:02 +0100

    Eurekaserver

--- src/main/java/com/example/eurekaserveraa/EurekaserveraaApplication.java
+++ src/main/java/com/example/eurekaserveraa/EurekaserveraaApplication.java
@@ -3,7 +3,9 @@ package com.example.eurekaserveraa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @EnableEurekaServer
 @SpringBootApplication
